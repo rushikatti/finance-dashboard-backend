04a5600ac15d86c68963b61390d9aa1ddb53a211
Rushi Katti <rushi111011@gmail.com>
2026-04-03 16:36:39 +0530

    fix: module name issue

--- backend_config/settings.py
+++ backend_config/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "finance_backend.urls"
+ROOT_URLCONF = 'backend_config.urls'
 
 TEMPLATES = [
     {
