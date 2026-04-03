396594cbcdba0b1d4970413c30fb382954a2e5cb
Rushi Katti <rushi111011@gmail.com>
2026-04-03 17:08:12 +0530

    fix: add home function

--- backend_config/urls.py
+++ backend_config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend is running 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
